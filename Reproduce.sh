e9b305a4bfcd9c99ac1d581850852774a0519f52
luoyi-hi <2479679585@qq.com>
2025-08-08 10:59:55 +0800

    main

---
+++ Reproduce.sh
@@ -0,0 +1,4 @@
+python main-master/experiments/evaluate.py -cfg  FaST/sd_96_48.py -ckpt Parameters_FaST/sd/96_48/FaST_best_val_MAE.pt -g 0
+python main-master/experiments/evaluate.py -cfg  FaST/sd_96_96.py -ckpt Parameters_FaST/sd/96_96/FaST_best_val_MAE.pt -g 0
+python main-master/experiments/evaluate.py -cfg  FaST/sd_96_192.py -ckpt Parameters_FaST/sd/96_192/FaST_best_val_MAE.pt -g 0
+python main-master/experiments/evaluate.py -cfg  FaST/sd_96_672.py -ckpt Parameters_FaST/sd/96_672/FaST_best_val_MAE.pt -g 0
